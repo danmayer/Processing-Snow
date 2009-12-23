168c3128456fa354e4418de4a1cd52be38d32896
Dan Mayer <Danmayer@gmail.com>
2009-12-22 21:31:30 -0500

    first files added

---
+++ penrose_snowflake.rb
@@ -0,0 +1,65 @@
+class PenroseSnowflake
+  include Processing::Proxy
+
+  attr_accessor :axiom, :rule, :start_length, :theta, :production, :draw_length, 
+    :repeats, :xpos, :ypos
+  DELTA = (Math::PI/180) * 18.0 # convert degrees to radians
+
+  def initialize(xpos = 500, ypos = 500, size = 75)
+    @axiom        = "F3-F3-F3-F3-F"
+    @rule         = "F3-F3-F45-F++F3-F"
+    @start_length = size
+    @size         = size
+    @theta        = 0
+    @xpos         = xpos
+    @ypos         = ypos
+    reset
+  end
+
+  def reset # initialize or reset variables
+    @production  = axiom
+    @draw_length = start_length
+  end
+
+  ##############################################################################
+  # Not strictly in the spirit of either processing or L-Systems in my iterate
+  # function I have ignored the processing translate/rotate functions in favour
+  # of the direct calculation of the new x and y positions, thus avoiding such
+  # affine transformations.
+  ##############################################################################
+  def iterate(prod_, rule_)
+    new_production = ""
+    production.split("").each do |prod|
+      new_production. << rule if prod == 'F'
+      new_production. << prod if prod != 'F'
+    end
+    @draw_length *=  0.4
+    return new_production
+  end
+  
+  def render()
+    repeats = 1
+    production.split("").each do |element|
+      case element
+      when 'F'
+        line(xpos, ypos, (@xpos -= draw_length * repeats * Math.cos(theta)), (@ypos += draw_length * repeats * Math.sin(theta)))
+        repeats = 1
+      when '+'
+        @theta += DELTA * repeats
+        repeats = 1
+      when '-'
+        @theta -= DELTA * repeats
+        repeats = 1
+      when '3', '4', '5'
+        repeats += Integer(element)
+      else puts "Grammar not recognized"  
+      end
+    end
+  end
+
+  def simulate gen
+    gen.times do      
+      @production = iterate(production, rule)         
+    end
+  end
+end
---
+++ snow.rb
@@ -0,0 +1,46 @@
+require 'penrose_snowflake'
+
+class Sketch < Processing::App
+  def new(params)
+    @width = params[:width]
+    @height = params[:height]
+    super
+  end
+
+  def setup
+    @background = 0
+    background @background
+    smooth
+    
+    buffer_x  = 10
+    buffer_y  = 10
+    @diameter = 72 
+  end
+
+  def draw
+    width = get_rand(20,@diameter)
+    x_snow = get_rand(width+20,@width) 
+    y_snow = get_rand(width+20,@height)
+    stroke 255
+    penrose = PenroseSnowflake.new(x_snow, y_snow, get_rand(20,width))
+    penrose.simulate get_rand(2,5)
+    penrose.render
+  end
+  
+  def mouse_pressed
+    background @background
+  end
+  
+  def key_pressed
+    if key == "s"
+      saveFrame "frame###.jpg"
+    end
+  end
+  
+  def get_rand(min = 0, max = 1000)
+    min + rand((max - min))
+  end
+  
+end
+
+Sketch.new(:width => 1024, :height => 768, :title => "Snow", :full_screen => true)
